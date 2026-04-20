0941259033f5ba2e5811bdd2bde3811678af7ed5
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-04-19 21:09:27 -0700

    Add tests for maxDiff

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -17,7 +17,34 @@ public class PracticeTest {
         // Largest: 9, Smallest: -5, Difference: 9 - -5 = 14
         assertEquals(14, actual);
     }
+
+    @Test
+    void testMaxWithNum0Only() {
+        int[] numbers = {3};
+        int actual = Practice.maxDiff(numbers);
+        assertEquals(0, actual);
+    }
     
+    @Test
+    void testMaxAt0() {
+        int[] numbers = {15, 5, 4, 3, 1, 0};
+        int actual = Practice.maxDiff(numbers);
+        assertEquals(15, actual);
+    }
+
+    @Test
+    void testMinAt0() {
+        int[] numbers = {0, 1, 3, 5, 7, 15};
+        int actual = Practice.maxDiff(numbers);
+        assertEquals(15, actual);
+    }
+
+    @Test
+    void testMaxWithRepeatedValue() {
+        int[] numbers = {3, 3, 3, 3, 3};
+        int actual = Practice.maxDiff(numbers);
+        assertEquals(0, actual);
+    }
 
     // TODO: Make tests for each problem you solve
     
